01d49d80e2976cf11f613f2a676c1bc73b056efe
Nathan Zion D'Penha <dpenhanathan@yahoo.in>
2017-09-16 14:31:22 +0530

    Add files via upload

---
+++ Java/assignment3/Enclass.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+
+
+
+class Enclass
+{
+	
+	void fun()
+	{
+		Scanner scanme=new Scanner(System.in);
+		
+		System.out.println("Input");
+		
+		String a=scanme.nextLine();
+		
+		
+		String en="";
+		
+		
+		for(int i=0;i<a.length();i++)
+		{
+			
+			char st=a.charAt(i);
+	
+			if((st<123 &&st>96)||(st>64&&st<91))
+			{
+				
+				st=(char)((int)st+1);
+				
+				if(st==123)
+				{
+					st='a';
+				}
+				if(st==91)
+				{
+					st='A';
+				}
+				
+				
+			}
+			
+			en=en+st;
+		}
+		
+		
+		System.out.println(en);
+		
+		
+		
+		
+		
+		
+	}
+	
+	public static void main(String args[])
+	{
+		Enclass obj=new Enclass();
+		obj.fun();
+		
+	}
+}
