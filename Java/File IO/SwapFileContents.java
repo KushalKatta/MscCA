1dea83f4984c6e16e28b43905b104aef468c77d2
Kushal Katta <kushalkatta16@gmail.com>
2017-09-19 11:08:56 +0530

    Java

--- Java/File IO/SwapFileContents.java
+++ Java/File IO/SwapFileContents.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 class Main
 {
     public static void main(String[] args) {
@@ -6,7 +8,13 @@ class Main
         
         String file1 = fo.readString("Enter first File Name");
         String file2 = fo.readString("Enter second File Name");
+        String fileTemp = "fileTemp";
         
-        fo.OneFileToAnotherFile(file1, file2);
+        fo.OneFileToAnotherFile(file1, fileTemp);
+        fo.OneFileToAnotherFile(file2, file1);
+        fo.OneFileToAnotherFile(fileTemp, file2);
+
+        File myFile = new File(fileTemp);
+        myFile.delete();
     }
 }
