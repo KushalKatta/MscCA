6f47ed702582690e07f7471dad4efcc83ea8ed02
ar0ra1 <akash.arora777@gmail.com>
2017-09-21 08:45:43 +0530

    Fixed Bugs

--- WE/myBatch.bat
+++ WE/myBatch.bat
@@ -1,7 +1,9 @@
 ::: Author - Akash Arora
+::: MSc(CA) 2017-19
+::: 17030142001
 @echo off
 
-::: LOG FILE PATH - Desktop 
+::: LOG FILE PATH - Current Logged In User's Desktop
 set logPath=%USERPROFILE%\Desktop\mylog.log
 
 ::: Taking Program Name from User (Case Sensitive)
@@ -19,34 +21,34 @@ set /p searchApp=Enter Program To Look for :
 	if not %verboseLevel%==Normal if not %verboseLevel%==Verbose echo. && echo Invalid Choice... Retry && goto :while
 
 ::: LOG FILE FORMAT PREFIX
-set logPrefix=[%DATE% %TIME%] : %verboseLevel% :
+set logPrefix= : %verboseLevel% :
 
-echo %logPrefix% START LOG	 > %logPath%
+echo [%DATE% %TIME%] %logPrefix% START LOG	 > %logPath%
 echo -------------------------- >> %logPath%
 echo Starting Script
 echo.
 echo Running Set Command
-echo %logPrefix% Running Set Command >> %logPath%
+echo [%DATE% %TIME%] %logPrefix% Running Set Command >> %logPath%
 
 echo --------------------------
 echo -------------------------- >> %logPath%
 set
-if %verboseLevel%==Verbose echo %logPrefix% Before Adding myName >> %logPath% && echo. >> %logPath% && set >> %logPath%
+if %verboseLevel%==Verbose echo [%DATE% %TIME%] %logPrefix% Before Adding myName >> %logPath% && echo. >> %logPath% && set >> %logPath%
 echo --------------------------
 echo -------------------------- >> %logPath%
 
 echo Setting myName = Akash
-echo %logPrefix% Setting myName = Akash >> %logPath%
+echo [%DATE% %TIME%] %logPrefix% Setting myName = Akash >> %logPath%
 set myName=Akash
 
 echo --------------------------
 echo Running Set Command
-echo %logPrefix% Running Set Command >> %logPath%
+echo [%DATE% %TIME%] %logPrefix% Running Set Command >> %logPath%
 echo --------------------------
 echo -------------------------- >> %logPath%
 set
 ::: Checking for Verbositiy Level and Writing to log file
-if %verboseLevel%==Verbose echo %logPrefix% After Adding myName >> %logPath% && echo. >> %logPath% && set >> %logPath%
+if %verboseLevel%==Verbose echo [%DATE% %TIME%] %logPrefix% After Adding myName >> %logPath% && echo. >> %logPath% && set >> %logPath%
 echo --------------------------
 echo -------------------------- >> %logPath%
 
@@ -54,12 +56,13 @@ echo -------------------------- >> %logPath%
 set counter=0
 set foundWinRAR=false
 
-echo %logPrefix% Looking for Software %searchApp% >> %logPath%
+echo [%DATE% %TIME%] %logPrefix% Looking for Software %searchApp% >> %logPath%
 echo -------------------------- >> %logPath%
 
 ::: Searching Begins in ProgramFiles
 echo Searching In ProgramFiles
-echo %logPrefix% Searching In ProgramFiles  >> %logPath%
+echo [%DATE% %TIME%] %logPrefix% Searching In ProgramFiles  >> %logPath%
+echo -------------------------- >> %logPath%
 
 ::: Changing Directory
 cd /d %ProgramFiles%
@@ -68,46 +71,50 @@ echo **************************
 
 ::: Checking the name of each Directory
 for /d  %%i in (*) do (
-	if %verboseLevel%==Verbose echo %logPrefix% Checking Directory Name = %%i >> %logPath%
+	if %verboseLevel%==Verbose echo [%DATE% %TIME%] %logPrefix% Checking Directory Name = %%i >> %logPath%
 	if %%i==%searchApp% (
 		set counter=%counter%+1
 		if %%i==WinRAR set foundWinRAR=true
 		echo %searchApp% Found
-		echo %logPrefix% %searchApp% FOUND >> %logPath%
+		echo [%DATE% %TIME%] %logPrefix% %searchApp% FOUND >> %logPath%
 		)
 )
 echo -------------------------- >> %logPath%
 
-if %foundWinRAR%==true echo WinRAR was Found... UnInstalling Now. && echo %logPrefix% WinRAR Found .. Changing Directory and UnInstalling >> %logPath% && cd WinRAR && uninstall.exe
-if %counter%==0 echo %searchApp% was not found in %ProgramFiles% && echo %logPrefix% %searchApp% was not found in %ProgramFiles% >> %logPath%
+if %foundWinRAR%==true echo WinRAR was Found... UnInstalling Now. && echo [%DATE% %TIME%] %logPrefix% WinRAR Found .. Changing Directory and UnInstalling >> %logPath% && cd WinRAR && uninstall.exe
+if %counter%==0 echo %searchApp% was not found in %ProgramFiles% && echo [%DATE% %TIME%] %logPrefix% %searchApp% was not found in %ProgramFiles% >> %logPath%
 
 echo --------------------------
-echo -------------------------- >> %logPath%
+
+::: Re-initialising the variables
+set counter=0
+set foundWinRAR=false
 
 ::: Searching Begins in ProgramFiles(x86)
 echo Searching In ProgramFiles(x86)
-echo %logPrefix% Searching In ProgramFiles(x86) >> %logPath%
+echo [%DATE% %TIME%] %logPrefix% Searching In ProgramFiles(x86) >> %logPath%
+echo -------------------------- >> %logPath%
 cd /d %ProgramFiles(x86)%
 
 echo **************************
 
 ::: Checking the name of each Directory
 for /d  %%i in (*) do (
-	if %verboseLevel%==Verbose echo %logPrefix% Checking Directory Name = %%i >> %logPath%
+	if %verboseLevel%==Verbose echo [%DATE% %TIME%] %logPrefix% Checking Directory Name = %%i >> %logPath%
 	if %%i==%searchApp% (
 		set counter=%counter%+1
 		if %%i==WinRAR set foundWinRAR=true
 		echo %searchApp% Found
-		echo %logPrefix% %searchApp% FOUND >> %logPath%
+		echo [%DATE% %TIME%] %logPrefix% %searchApp% FOUND >> %logPath%
 		)
 )
 echo -------------------------- >> %logPath%
-if %foundWinRAR%==true echo WinRAR was Found... UnInstalling Now. && echo %logPrefix% WinRAR Found .. Changing Directory and UnInstalling >> %logPath% && cd WinRAR && uninstall.exe
-if %counter%==0 echo %searchApp% was not found in %ProgramFiles(x86)% && echo %logPrefix% %searchApp% was not found in %ProgramFiles% >> %logPath%
+if %foundWinRAR%==true echo WinRAR was Found... UnInstalling Now. && echo [%DATE% %TIME%] %logPrefix% WinRAR Found .. Changing Directory and UnInstalling >> %logPath% && cd WinRAR && uninstall.exe
+if %counter%==0 echo %searchApp% was not found in %ProgramFiles(x86)% && echo [%DATE% %TIME%] %logPrefix% %searchApp% was not found in %ProgramFiles% >> %logPath%
 
 ::: Ending
 echo --------------------------
 echo -------------------------- >> %logPath%
-echo %logPrefix% END LOG >> %logPath%
+echo [%DATE% %TIME%] %logPrefix% END LOG >> %logPath%
 
 pause
