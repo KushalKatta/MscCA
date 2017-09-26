b182164f31530c9281996f64afb46a107e3356d6
Kushal Katta <kushalkatta16@gmail.com>
2017-09-26 23:36:01 +0530

    Updated LayourTypes.java Java Swings

--- Java/LayoutTypes.java
+++ Java/LayoutTypes.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 class Layouts extends JFrame
 {
     JLabel label1;
+    JPanel welcomePanel;
 
     JLabel labelName;
     JTextField textName;
@@ -22,28 +23,37 @@ class Layouts extends JFrame
 
     Layouts()
     {
+        welcomePanel = new JPanel();
         label1 = new JLabel("Welcome To This Software!");
+        welcomePanel.add(label1);
         
         panelName = new JPanel();
-        panelName.setLayout(new BorderLayout());
         labelName = new JLabel("Name");
         textName = new JTextField(15);
-        panelName.add(labelName, BorderLayout.WEST);
-        panelName.add(textName, BorderLayout.EAST);
-
+        panelName.add(labelName);
+        panelName.add(textName);
+        // panelName.setLayout(new BorderLayout());
+        // panelName.add(labelName, BorderLayout.WEST);
+        // panelName.add(textName, BorderLayout.EAST);
+        
         panelAddress = new JPanel();
-        panelAddress.setLayout(new BorderLayout());
         labelAddress = new JLabel("Address");
         textAddress = new JTextField(15);
-        panelAddress.add(labelAddress, BorderLayout.WEST);
-        panelAddress.add(textAddress, BorderLayout.EAST);
-
+        panelAddress.add(labelAddress);
+        panelAddress.add(textAddress);
+        // panelAddress.setLayout(new BorderLayout());
+        // panelAddress.add(labelAddress, BorderLayout.WEST);
+        // panelAddress.add(textAddress, BorderLayout.EAST);
+        
         panelButton = new JPanel();
-        panelButton.setLayout(new BorderLayout());
         btnSubmit = new JButton("Submit");
         btnCancel = new JButton("Cancel");
-        panelButton.add(btnSubmit, BorderLayout.WEST);
-        panelButton.add(btnCancel, BorderLayout.EAST);
+        panelButton.add(btnSubmit);
+        panelButton.add(btnCancel);
+        
+        // panelButton.setLayout(new BorderLayout());
+        // panelButton.add(btnSubmit, BorderLayout.WEST);
+        // panelButton.add(btnCancel, BorderLayout.EAST);
     }
 
     
@@ -52,12 +62,11 @@ class Layouts extends JFrame
         obj.setSize(400,300);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(Layouts.EXIT_ON_CLOSE);
+        
         obj.setLayout(new BorderLayout());
 
+        obj.add(obj.welcomePanel, BorderLayout.NORTH);
 
-        obj.add(obj.label1, BorderLayout.NORTH);
-
-        // BoxLayout centerBox = new BoxLayout(BoxLayout.Y_AXIS);
         JPanel centerPane=new JPanel();
         centerPane.setLayout(new BoxLayout(centerPane, BoxLayout.Y_AXIS));
         centerPane.add(obj.panelName);
