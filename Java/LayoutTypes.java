30f856d0f3c4fcae01faab7e3a6ffbf74a111e29
Mihir Vohra <vohra.mihir@yahoo.com>
2017-09-27 12:53:26 +0530

    fixed layout screen

--- Java/LayoutTypes.java
+++ Java/LayoutTypes.java
@@ -1,78 +1,123 @@
-/*  Types of Layout   */
+//Java program to create an input form using various swing controls and layouts
 
-import java.awt.*;
-import java.awt.event.*;
+//Date - 26 September 2017
+
 import javax.swing.*;
+import java.awt.*;
 
-class Layouts extends JFrame
-{
-    JLabel label1;
-    JPanel welcomePanel;
+//begin class
+class LayoutTypes extends JFrame {
+	// controls of the class
+	JLabel lbl_welcome, lbl_name, lbl_address, lbl_gender, lbl_country, lbl_language;
+	JButton btn_submit, btn_cancel;
+	JTextField txt_name;
+	JTextArea txta_address;
+	ButtonGroup bg_gender;
+	JRadioButton opt_male, opt_female;
+	JComboBox<String> combo_country;
+	JCheckBox chk_c, chk_cpp, chk_java;
+	JPanel panel_main, panel_buttons, panel_language, panel_country, panel_gender, panel_address, panel_name,
+			panel_welcome;
+	Dimension dimension;
 
-    JLabel labelName;
-    JTextField textName;
-    JPanel panelName;
+	// data members of the class
+	static final String countries[] = { "India", "Australia", "South Africa", "USA", "Srilanka" };
 
-    JLabel labelAddress;
-    JTextField textAddress;
-    JPanel panelAddress;
+	// parameterised constructor
+	LayoutTypes(String title) {
+		// title of the form
+		super(title);
 
-    JButton btnSubmit;
-    JButton btnCancel;
-    JPanel panelButton;
+		// dimensions of client screen
+		dimension = Toolkit.getDefaultToolkit().getScreenSize();
 
-    Layouts()
-    {
-        welcomePanel = new JPanel();
-        label1 = new JLabel("Welcome To This Software!");
-        welcomePanel.add(label1);
-        
-        panelName = new JPanel();
-        labelName = new JLabel("Name");
-        textName = new JTextField(15);
-        panelName.add(labelName);
-        panelName.add(textName);
-        // panelName.setLayout(new BorderLayout());
-        // panelName.add(labelName, BorderLayout.WEST);
-        // panelName.add(textName, BorderLayout.EAST);
-        
-        panelAddress = new JPanel();
-        labelAddress = new JLabel("Address");
-        textAddress = new JTextField(15);
-        panelAddress.add(labelAddress);
-        panelAddress.add(textAddress);
-        // panelAddress.setLayout(new BorderLayout());
-        // panelAddress.add(labelAddress, BorderLayout.WEST);
-        // panelAddress.add(textAddress, BorderLayout.EAST);
-        
-        panelButton = new JPanel();
-        btnSubmit = new JButton("Submit");
-        btnCancel = new JButton("Cancel");
-        panelButton.add(btnSubmit);
-        panelButton.add(btnCancel);
-        
-        // panelButton.setLayout(new BorderLayout());
-        // panelButton.add(btnSubmit, BorderLayout.WEST);
-        // panelButton.add(btnCancel, BorderLayout.EAST);
-    }
+		// initialising the controls
+		lbl_welcome = new JLabel("Welcome to my world!");
+		lbl_name = new JLabel("Name:");
+		lbl_address = new JLabel("Address:");
+		lbl_gender = new JLabel("Gender:");
+		lbl_country = new JLabel("Country:");
+		lbl_language = new JLabel("Language:");
 
-    
-    public static void main(String[] args) {
-        Layouts obj = new Layouts();
-        obj.setSize(400,300);
-        obj.setVisible(true);
-        obj.setDefaultCloseOperation(Layouts.EXIT_ON_CLOSE);
-        
-        obj.setLayout(new BorderLayout());
+		btn_submit = new JButton("Submit");
+		btn_cancel = new JButton("Cancel");
 
-        obj.add(obj.welcomePanel, BorderLayout.NORTH);
+		opt_male = new JRadioButton("Male");
+		opt_female = new JRadioButton("Female");
 
-        JPanel centerPane=new JPanel();
-        centerPane.setLayout(new BoxLayout(centerPane, BoxLayout.Y_AXIS));
-        centerPane.add(obj.panelName);
-        centerPane.add(obj.panelAddress);
-        obj.add(centerPane, BorderLayout.CENTER);
+		chk_c = new JCheckBox("C");
+		chk_cpp = new JCheckBox("C++");
+		chk_java = new JCheckBox("Java");
 
-        obj.add(obj.panelButton, BorderLayout.SOUTH);
-    }
-}
+		txt_name = new JTextField(10);
+
+		txta_address = new JTextArea(5, 20);
+
+		combo_country = new JComboBox<String>(countries);
+
+		panel_main = new JPanel();
+		panel_buttons = new JPanel();
+		panel_language = new JPanel();
+		panel_country = new JPanel();
+		panel_gender = new JPanel();
+		panel_address = new JPanel();
+		panel_name = new JPanel();
+		panel_welcome = new JPanel();
+
+		bg_gender = new ButtonGroup();
+
+		// button group for radio buttons
+		bg_gender.add(opt_male);
+		bg_gender.add(opt_female);
+
+		// properties of the frame
+		setSize(dimension.width, dimension.height); // full screen
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+
+		// setting layouts of panels
+		panel_main.setLayout(new BoxLayout(panel_main, BoxLayout.PAGE_AXIS));
+
+		// adding controls to the panels
+		// welcome label
+		panel_welcome.add(lbl_welcome);
+		// name
+		panel_name.add(lbl_name);
+		panel_name.add(txt_name);
+		// Address
+		panel_address.add(lbl_address);
+		panel_address.add(txta_address);
+		// Gender
+		panel_gender.add(lbl_gender);
+		panel_gender.add(opt_male);
+		panel_gender.add(opt_female);
+		// Country
+		panel_country.add(lbl_country);
+		panel_country.add(combo_country);
+		// Language
+		panel_language.add(lbl_language);
+		panel_language.add(chk_c);
+		panel_language.add(chk_cpp);
+		panel_language.add(chk_java);
+		// Buttons
+		panel_buttons.add(btn_submit, BorderLayout.WEST);
+		panel_buttons.add(btn_cancel, BorderLayout.EAST);
+
+		// adding sub panels to the main panel
+		panel_main.add(panel_welcome, BorderLayout.NORTH);
+		panel_main.add(panel_name);
+		panel_main.add(panel_address);
+		panel_main.add(panel_gender);
+		panel_main.add(panel_country);
+		panel_main.add(panel_language);
+		panel_main.add(panel_buttons, BorderLayout.SOUTH);
+
+		// adding the main panel to the frame
+		add(panel_main, BorderLayout.CENTER);
+	}// end parameterised constructor
+
+	// main method
+	public static void main(String[] args) {
+		LayoutTypes form1 = new LayoutTypes("Input form with various controls and layouts");
+	}// end main
+}// end class
